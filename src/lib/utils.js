aa46a7ab372ef0269db1fcfa7685ce567856d85e
Andres Umanzor <andres.umanzor@hotmail.com>
2020-01-14 14:33:36 -0600

    adding loader to utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const inquirer = require('inquirer');
 
 const camelize = str => {
     str = str.replace(/[0-9]/g, '');
@@ -9,9 +10,23 @@ const warningLog = str => console.log(chalk.yellow(`--- ${str}`))
 const successLog = str => console.log(chalk.green(`--- ${str}`))
 const errorLog = str => console.log(chalk.red(`--- ${str}`))
 
+const loader = (loadingText) => {
+    this.loader = [`/ ${loadingText}`, `| ${loadingText}`, `\\ ${loadingText}`, `- ${loadingText}`];
+    this.i = loader.length;
+    this.loadingBar = new inquirer.BottomBar({ bottomBar: loader[this.i % 4] });
+    this.refreshInterval = setInterval(() => {
+        this.loadingBar.updateBottomBar(loader[this.i++ % 4]);
+    }, 300);
+    this.end = (endingText) => {
+        clearInterval(this.refreshInterval);
+        this.loadingBar.updateBottomBar(`${endingText}\n`);
+    }
+}
+
 module.exports = {
     camelize,
     warningLog,
     successLog,
-    errorLog
+    errorLog,
+    loader
 };
